642a0eb756efd71a462b15dd7a9b0aa380ea157d
Alex <33612197+alexc83@users.noreply.github.com>
2024-09-29 08:51:44 +0000

    Up through video 2

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+//import React from 'react'
+import 'tachyons'
+import PropTypes from 'prop-types';
+
+const Card = ({ name, email, id}) => {
+    return (
+        <div className="tc black bg-light-green dib br3 pa3 ma2 grow bw2 shadow5">
+            <img src={`https://robohash.org/${id}}?200x200`} alt="photo"/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import CardList from './CardList.jsx'
 import './index.css'
+import {robots} from './robots.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <App />
+    <CardList robots={robots}/>
   </StrictMode>,
 )
