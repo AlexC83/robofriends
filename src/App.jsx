12429c470e386dcccac1fac4afde101a127ac113
Alex <33612197+alexc83@users.noreply.github.com>
2024-09-29 04:36:19 +0000

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLodddgo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+class App extends React.Component {
+  //const [count, setCount] = useState(0)
+  render() {
   return (
     <>
       <div>
@@ -26,10 +26,11 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more!
       </p>
     </>
   )
+}
 }
 
 export default App
